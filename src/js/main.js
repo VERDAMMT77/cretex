128dacc77b6b7f49c01b25dc4b445ea0da95cdaf
rozmovva <mriza4448@gmail.com>
2023-12-23 18:53:53 +0600

    add:project

--- src/js/main.js
+++ src/js/main.js
@@ -8,7 +8,40 @@ const swiper = new Swiper(".team__swiper", {
     autoplay:  {
         delay: 2000
     },
-    loop: true
+    loop: true,
+    breakpoints: {
+        886: {
+            slidesPerView: 3,
+        },
+
+        768: {
+            slidesPerView: 3,
+        },
+
+        676: {
+            slidesPerView: 2,
+        },
+
+        576: {
+            slidesPerView: 2,
+        },
+
+        500: {
+            slidesPerView: 2,
+        },
+
+        425: {
+            slidesPerView: 1,
+        },
+
+        375: {
+            slidesPerView: 1,
+        },
+
+        320: {
+            slidesPerView: 1,
+        },
+    }
 });
 
 const swiper2 = new Swiper(".say__swiper", {
@@ -37,6 +70,20 @@ $(function() {
     });
 
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    let header = document.querySelector(".header")
+    window.addEventListener("scroll", function () {
+        let scrollPosition = window.scrollY
+        if (scrollPosition > 900){
+            header.style.background = "white"
+        }
+        else {
+            header.style.background = "#ffdad5"
+        }
+    })
+})
+
 let burger = document.querySelector('.header__burger')
 let burgerMenu = document.querySelector('.header__burgerMenu')
 
@@ -45,3 +92,25 @@ burger.addEventListener('click',() => {
     burger.classList.toggle('active')
     burgerMenu.classList.toggle('active')
 })
+
+
+let openOverlay = document.querySelector('.header__register')
+let overlay = document.querySelector('.overlay')
+let closeOverlay = document.querySelector('.close')
+
+
+openOverlay.addEventListener('click', () => {
+    overlay.style.display = 'flex'
+})
+
+
+closeOverlay.addEventListener('click', () => {
+    overlay.style.display = 'none'
+})
+
+overlay.addEventListener('click', (e) => {
+    if (e.target.className === 'overlay') overlay.style.display='none'
+    // overlay.style.display = 'none'
+})
+
+
